716c275c6fa9ba8b699b029acc8064cd2cbcabbf
KittNeKit <nikiyorkovich@gmail.com>
2023-12-04 19:22:00 +0200

    fix bugs

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
 class Pagination:
-    @classmethod
+    @staticmethod
     def create_pagination(
-            cls,
-            current_page: int,
-            total_pages: int,
-            boundaries: int,
-            around: int
+        current_page: int, total_pages: int, boundaries: int, around: int
     ) -> None:
-        first_boundary = cls.boundary_generator(1, boundaries)
-        last_boundary = cls.boundary_generator(total_pages - boundaries + 1, total_pages)
+        first_boundary = Pagination._generate_first_boundary_or_print_result(
+            boundaries, total_pages
+        )
+        if first_boundary is None:
+            return
+
+        last_boundary = Pagination._generate_last_boundary_or_print_result(
+            first_boundary, boundaries, total_pages
+        )
+        if last_boundary is None:
+            return
 
         before_current_page = max(current_page - around, boundaries + 1)
         after_current_page = min(current_page + around, total_pages - boundaries)
-        around_current_page = cls.boundary_generator(before_current_page, after_current_page)
+        around_current_page = Pagination.boundary_generator(
+            before_current_page, after_current_page
+        )
 
         pagination = first_boundary
-        pagination += ["..."] * (before_current_page > boundaries + 1)
+        pagination += ["..."] if before_current_page > boundaries + 1 else ""
+
         pagination += around_current_page
-        pagination += ["..."] * (after_current_page < total_pages - boundaries)
+        pagination += ["..."] if after_current_page < total_pages - boundaries else ""
+
         pagination += last_boundary
 
         print(" ".join(map(str, pagination)))
+
+    @staticmethod
+    def _generate_first_boundary_or_print_result(
+        boundaries: int, total_pages: int
+    ) -> list | None:
+        if boundaries > total_pages:
+            result = Pagination.boundary_generator(1, total_pages)
+            print(" ".join(map(str, result)))
+            return
+        else:
+            return Pagination.boundary_generator(1, boundaries)
+
+    @staticmethod
+    def _generate_last_boundary_or_print_result(
+        first_boundary: list, boundaries: int, total_pages: int
+    ) -> list | None:
+        if boundaries > total_pages / 2:
+            last_boundary = Pagination.boundary_generator(boundaries + 1, total_pages)
+            print(" ".join(map(str, first_boundary + last_boundary)))
+            return
+        else:
+            return Pagination.boundary_generator(
+                total_pages - boundaries + 1, total_pages
+            )
 
     @staticmethod
     def boundary_generator(start: int, end: int) -> list:
